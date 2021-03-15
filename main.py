387fae80e134582507c52e22d260771089c790e4
Artyom Pugachev <artempugachev1@gmail.com>
2021-03-15 22:46:10 +0300

    hello bot

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import requests
+from telegram.ext import Updater, CommandHandler
+import logging
 
-from secret import API_KEY
+from secret import API_KEY, TOKEN
 import requester
 
 SPY = requester.global_quote('SPY')
 
 print(SPY['price'])
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=SPY['price'])
+
+updater = Updater(token=TOKEN, use_context=True)
+
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
+updater.idle()
